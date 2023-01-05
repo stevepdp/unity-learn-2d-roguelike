181d8a9c12b868714e49450ab951f18cf10dd5c9
Stephen Pearce <hello@stevepdp.dev>
2023-01-05 17:37:13 +0000

    Add a game loader script

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+    public static GameManager instance = null; //singleton pattern
     public BoardManager boardScript;
     private int level = 3; // test level 3 first, because that's where the enemies will appear
 
     void Awake()
     {
+        //singleton pattern
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);
+
         boardScript = GetComponent<BoardManager>();
         InitGame();
     }

dc7ee0a2211fdd9d5b73ad339063f694fabc2395
Stephen Pearce <hello@stevepdp.dev>
2023-01-06 16:48:15 +0000

    Add audio and SoundManager

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,6 +11,13 @@ public class Player : MovingObject {
 	public int pointsPerSoda = 20;
 	public float restartLevelDelay = 1f;
 	public Text foodText;
+	public AudioClip moveSound1;
+	public AudioClip moveSound2;
+	public AudioClip eatSound1;
+	public AudioClip eatSound2;
+	public AudioClip drinkSound1;
+	public AudioClip drinkSound2;
+	public AudioClip gameOverSound;
 
 	private Animator animator;
 	private int food;
@@ -56,6 +63,10 @@ public class Player : MovingObject {
 		base.AttemptMove<T>(xDir, yDir);
 
 		RaycastHit2D hit;
+		if (Move (xDir, yDir, out hit))
+        {
+			SoundManager.instance.RandomizeSfx(moveSound1, moveSound2);
+        }
 
 		CheckIfGameOver();
 
@@ -73,12 +84,14 @@ public class Player : MovingObject {
         {
 			food += pointsPerFood;
 			foodText.text = "+" + pointsPerFood + "  Food: " + food;
+			SoundManager.instance.RandomizeSfx(eatSound1, eatSound2);
 			other.gameObject.SetActive(false);
         }
 		else if (other.tag == "Soda")
         {
 			food += pointsPerSoda;
 			foodText.text = "+" + pointsPerSoda + "  Soda: " + food;
+			SoundManager.instance.RandomizeSfx(drinkSound1, drinkSound2);
 			other.gameObject.SetActive(false);
         }
 	}
@@ -106,6 +119,10 @@ public class Player : MovingObject {
     private void CheckIfGameOver()
     {
 		if (food <= 0)
+        {
+			SoundManager.instance.PlaySingle(gameOverSound);
+			SoundManager.instance.musicSource.Stop();
 			GameManager.instance.GameOver();
+		}
 	}
 }
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+	public AudioSource efxSource;
+	public AudioSource musicSource;
+	public static SoundManager instance = null; // this is a singleton
+
+	public float lowPitchRange = .95f; // -5% variation
+	public float highPitchRange = 1.05f; // +5% variation
+
+	void Awake()
+	{
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy(gameObject);
+
+		DontDestroyOnLoad(gameObject);
+	}
+
+	public void PlaySingle(AudioClip clip)
+    {
+		efxSource.clip = clip;
+		efxSource.Play();
+    }
+
+	public void RandomizeSfx(params AudioClip[] clips) // params lets you pass a comma-seperated list of args of same type.
+    {
+		int randomIndex = Random.Range(0, clips.Length);
+		float randomPitch = Random.Range(lowPitchRange, highPitchRange);
+
+		efxSource.pitch = randomPitch;
+		efxSource.clip = clips[randomIndex];
+		efxSource.Play();
+    }
+}
